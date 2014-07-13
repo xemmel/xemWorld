2e464b4178994d82683162070c3f0aa1011e439f
xemmel <lacour@gmail.com>
2014-07-13 20:42:04 +0200

    data moved to file

--- js/FirstModule.js
+++ js/FirstModule.js
@@ -1,13 +1,19 @@
 var app = angular.module("FirstModule", []);
 
-app.controller("FirstController",function($scope) {
+app.controller("FirstController",function($scope,$http) {
 
-$scope.persons = [
+/*$scope.persons = [
 	{id:1,name:"Clara",Born:"2010-07-21"},
 	{id:2,name:"Morten",Born:"2010-07-21"},
 	{id:3,name:"Mikkel",Born:"2010-07-21"},
 	{id:4,name:"Mogens",Born:"2010-07-21"}
-];
+];*/
+
+$http.get("data/Persons.data").
+	success(function(data) {
+		$scope.persons = data;
+	});
+
 console.log($scope.persons);
 
 });
