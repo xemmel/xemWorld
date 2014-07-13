6841497307f343d739cec44bff0ff2047113ae07
xemmel <lacour@gmail.com>
2014-07-13 20:35:42 +0200

    More updates

--- js/FirstModule.js
+++ js/FirstModule.js
@@ -1,5 +1,13 @@
 var app = angular.module("FirstModule", []);
 
 app.controller("FirstController",function($scope) {
-$scope.nameInput = "Clara";
+
+$scope.persons = [
+	{id:1,name:"Clara",Born:"2010-07-21"},
+	{id:2,name:"Morten",Born:"2010-07-21"},
+	{id:3,name:"Mikkel",Born:"2010-07-21"},
+	{id:4,name:"Mogens",Born:"2010-07-21"}
+];
+console.log($scope.persons);
+
 });
